77171f0290f91eaf9514ba25d3f531b421ed8776
Andoku <koob27@gmail.com>
2011-07-10 18:08:24 +0400

    added README

--- 036/036.rb
+++ 036/036.rb
@@ -1,5 +1,5 @@
 n = 1000000
-s = (1..n).inject(0) do |sum,elem|
+s = (1...n).inject(0) do |sum, elem|
   if (elem.to_s == elem.to_s.reverse) && (elem.to_s(2) == elem.to_s(2).reverse)
     sum + elem
   else
