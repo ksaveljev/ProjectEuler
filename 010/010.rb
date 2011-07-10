7643f0d24085db5c9b06e7f0d7d7aa5514295778
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2011-07-10 19:57:57 +0300

    010 solved

---
+++ 010/010.rb
@@ -0,0 +1,12 @@
+def sieve_upto(n)
+  sieve = (0..n).to_a
+  sieve[0] = sieve[1] = nil
+  sieve.each do |p|
+    next unless p
+    break if p * p > n
+    (p*p).step(n, p) { |m| sieve[m] = nil }
+  end
+  sieve.compact
+end
+
+puts sieve_upto(2_000_000).inject(0, &:+)
