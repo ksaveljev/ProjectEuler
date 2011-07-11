062c6ff280c12996e07c8ba2e0a197e66b6d474d
Viktar Basharymau <6alliapumob@gmail.com>
2011-07-11 05:27:58 -0700

    No need to reinvent the wheel. Ruby already have built-in Eratosthenes's Sieve implemented (require 'prime').

--- 007/007.rb
+++ 007/007.rb
@@ -1,12 +1,3 @@
-def sieve_upto(n)
-  sieve = (0..n).to_a
-  sieve[0] = sieve[1] = nil
-  sieve.each do |p|
-    next unless p
-    break if p * p > n
-    (p*p).step(n, p) { |m| sieve[m] = nil }
-  end
-  sieve.compact
-end
+require 'prime'
 
-puts sieve_upto(500_000)[10000]
+Prime.each(500_000).each_with_index.find{|_, i| i == 10_000}.first
