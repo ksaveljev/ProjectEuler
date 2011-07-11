4cfd34621d0659849a1cef9b8bbb52e7b7a64e21
Viktar Basharymau <6alliapumob@gmail.com>
2011-07-11 05:50:43 -0700

    PE-014: Use 'memoize' gem. It allows to write less cryptic code. Also, the chain_length function is pure now.

--- 014/014.rb
+++ 014/014.rb
@@ -1,20 +1,15 @@
-hash, max = Hash.new, [0,0]
-for elem in (1...1000000) do
-  k, a = 1, elem
-  while elem != 1
-    if hash.include?(elem) 
-      k += hash[elem]
-      break
-    else
-      if elem % 2 != 0
-        elem = 3*elem + 1
-      else
-        elem /= 2
-      end
-        k += 1
-    end
+require 'memoize'
+
+include Memoize
+
+def chain_length(start)
+  return 0 if start == 1
+  if start.odd?
+    1 + chain_length(3 * start + 1)
+  else
+    1 + chain_length(start / 2)
   end
-  hash[a] = k
-  max = [a,k]	if k > max[1]
 end
-puts max[0]
+memoize :chain_length
+
+(1...1_000_000).max_by {|x| chain_length x}
