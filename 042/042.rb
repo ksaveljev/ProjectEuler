b661353d27c8c23d570206c8522b3b7b361b9303
Viktar Basharymau <6alliapumob@gmail.com>
2011-07-12 01:39:33 -0700

    PE-042: cosmetic change. Renamed `triangle` variable to `triangles`.

--- 042/042.rb
+++ 042/042.rb
@@ -1,7 +1,7 @@
-triangle = Array.new(10_000) { |n| n * (n + 1) / 2 }
+triangles = Array.new(10_000) { |n| n * (n + 1) / 2 }
 
 def value(word)
   word.each_byte.inject(0) { |sum, b| sum += b - 'A'.ord + 1 }
 end
 
-puts gets(nil).split(/,/).collect { |str| value(str[1..-2]) }.count { |v| triangle.include?(v) }
+puts gets(nil).split(/,/).collect { |str| value(str[1..-2]) }.count { |v| triangles.include?(v) }
