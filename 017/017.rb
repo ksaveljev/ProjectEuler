8d7111c8ebfd21bb71922a948bca1676190a8d97
Viktar Basharymau <6alliapumob@gmail.com>
2011-07-12 04:56:20 -0700

    PE-017: refactor code a bit in order to get rid of nested blocks

--- 017/017.rb
+++ 017/017.rb
@@ -1,3 +1,3 @@
 require 'linguistics'
 
-puts (1..1000).inject(0) { |sum, n| sum += Linguistics::EN.numwords(n).split(//).count { |c| ('a'..'z').include?(c) } }
+puts (1..1000).map { |n| Linguistics::EN.numwords(n).split(//) }.flatten.count { |c| ('a'..'z').include?(c) } }
