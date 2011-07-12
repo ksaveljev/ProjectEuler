75999c6c1070ba008b201b9623f28cb583766c1b
Viktar Basharymau <6alliapumob@gmail.com>
2011-07-12 01:34:00 -0700

    PE-042: replace 'magic' `65` constant with `'A'.ord - 1`

--- 042/042.rb
+++ 042/042.rb
@@ -1,7 +1,7 @@
 triangle = Array.new(10_000) { |n| n * (n + 1) / 2 }
 
 def value(word)
-  word.each_byte.inject(0) { |sum, b| sum += b - 64 }
+  word.each_byte.inject(0) { |sum, b| sum += b - 'A'.ord + 1 }
 end
 
 puts gets(nil).split(/,/).collect { |str| value(str[1..-2]) }.select { |v| triangle.include?(v) }.count
