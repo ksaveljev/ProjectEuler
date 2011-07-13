9c872fd031f8d073c20988b8c9a69604fb6c88bd
Viktar Basharymau <6alliapumob@gmail.com>
2011-07-13 06:39:27 -0700

    052: express the same algorithm with idiomatic Ruby code

--- 052/052.rb
+++ 052/052.rb
@@ -1,14 +1,5 @@
-1.upto(200000) do |x|
-  found = true
-  2.upto(6) do |i|
-    unless x.to_s.split(//).sort.join == (i*x).to_s.split(//).sort.join
-      found = false
-      break
-    end
-  end
-
-  if found
-    puts x
-    break
-  end
+answer = (1..200000).find do |x|
+  (1..6).map { |i| (x*i).to_s.split(//).sort }.uniq.size == 1
 end
+
+puts answer
