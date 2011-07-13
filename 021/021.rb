8a444624e343081c17f8acdafe6aac30ff54091c
Viktar Basharymau <6alliapumob@gmail.com>
2011-07-13 06:25:55 -0700

    021: more "Rubic" way to select numbers from a collection by given criteria

--- 021/021.rb
+++ 021/021.rb
@@ -1,13 +1,10 @@
-amicable = []
-
 def divisors_sum(n)
   (1..n/2).select { |v| n % v == 0 }.inject(0, :+)
 end
 
-2.upto(9999) do |n|
+amicable = (2..9999).select do |n|
   sum = divisors_sum(n)
-  next if sum == n
-  amicable << n if n == divisors_sum(sum)
+  sum != n and n == divisors_sum(sum)
 end
 
 puts amicable.inject(0, :+)
