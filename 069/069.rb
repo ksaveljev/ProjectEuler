ca5c5f4b41fc7372636089cca2775d868cb67d72
Viktar Basharymau <6alliapumob@gmail.com>
2011-07-14 22:34:42 -0700

    069: use "max_by{...}" method instead of "each{...}.each_with_index.max + 1"

--- 069/069.rb
+++ 069/069.rb
@@ -4,4 +4,4 @@ def phi(n)
   n.prime_division.inject(n) { |result, d| result  - result / d.first }
 end
 
-p (1..1_000_000).map { |n| n.to_f / phi(n) }.each_with_index.max.last + 1
+p (1..1_000_000).max_by { |n| n.to_f / phi(n) }
