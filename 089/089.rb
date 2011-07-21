354cd04bb041b88e8d147b95e1f4d7bb48066e41
Viktar Basharymau <6alliapumob@gmail.com>
2011-07-21 11:53:14 +0300

    089: removed unnecessary `reject(&:empty?)`

--- 089/089.rb
+++ 089/089.rb
@@ -12,7 +12,6 @@ RomanNumerals.const_set(:MAX, 100500)
 print File.open('roman.txt').
         each_line.
         map(&:chomp).
-        reject(&:empty?).
         map{|roman| roman.size - roman.to_i_roman.to_s_roman.size }.
         inject(:+)
 
