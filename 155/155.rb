f4e8f2f587dd9c77381b88990da32b9af467143b
Viktar Basharymau <6alliapumob@gmail.com>
2011-07-21 13:52:34 +0300

    155 solved

---
+++ 155/155.rb
@@ -0,0 +1,21 @@
+require 'rational'
+require 'memoize'
+
+include Memoize
+
+def d(n)
+  return [Rational(1)] if n == 1
+  total = []
+  for i in 1...n
+    subset_1 = d(i)
+    subset_2 = d(n-i)
+    subset_1.product(subset_2).each do |c_1, c_2|
+      total << (c_1 + c_2)
+      total << (c_1 * c_2 / (c_1 + c_2))
+    end
+  end
+  total.uniq
+end
+memoize :d
+
+print (1..18).map{|n| d(n)}.flatten.uniq.size
